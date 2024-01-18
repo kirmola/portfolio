54353854c6e7d801862ffc2bf3091d8999c08b3b
Aman Rawat <ar.rawataman@gmail.com>
2024-01-18 20:08:58 +0530

    updated buildfile

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 # build_files.sh
 pip install -r requirements.txt
 
-python3.9 manage.py tailwind build
 # make migrations
 
 if [ $ENV = "prod" ]; then
+    python3.9 manage.py tailwind build
     python3.9 manage.py migrate
     python3.9 manage.py tailwind build
     python3.9 manage.py collectstatic --noinput
