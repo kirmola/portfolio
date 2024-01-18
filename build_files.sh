94b2cc6c87af67c30ee64f59ac4d1dd19546378c
Aman Rawat <ar.rawataman@gmail.com>
2024-01-18 19:49:31 +0530

    updated buildfile

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,6 @@ pip install -r requirements.txt
 
 # make migrations
 
-yum install openssl11 openssl11-devel
 if [ $ENV = "prod" ]; then
     python3.9 manage.py migrate
     python3.9 manage.py tailwind build
