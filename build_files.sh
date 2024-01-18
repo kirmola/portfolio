b9b34f8dd88d1289d73afbab3e27c6809dc68e14
Aman Rawat <ar.rawataman@gmail.com>
2024-01-18 12:38:59 +0530

    modified buildfile to check env

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,8 @@ pip install -r requirements.txt
 # make migrations
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
-python3.9 manage.py createsuperuser --noinput
+
+if [ $ENV = "prod" ]; then
+    python3.9 manage.py createsuperuser --noinput
+
+fi
