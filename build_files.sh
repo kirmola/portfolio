cb0290e402118794504e41b8c08695ed4b036a65
Aman Rawat <ar.rawataman@gmail.com>
2024-01-18 12:39:51 +0530

    Fixed Build Script

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,10 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
 
 if [ $ENV = "prod" ]; then
+    python3.9 manage.py migrate
+    python3.9 manage.py collectstatic --noinput
     python3.9 manage.py createsuperuser --noinput
 
 fi
