739315cbae3ed168072e6b4665a5bec8244efb19
Aman Rawat <ar.rawataman@gmail.com>
2024-01-18 15:35:11 +0530

    changed fine tune configs

--- moodyai/logics.py
+++ moodyai/logics.py
@@ -15,11 +15,12 @@ class GenerateResponse:
 
         def run(model, inputs):
             input = { "messages": inputs }
-            response = requests.post(f"{self.API_BASE_URL}{model}", headers=self.headers, json=input)
+            session = requests.Session()
+            response = session.post(f"{self.API_BASE_URL}{model}", headers=self.headers, json=input)
             return response.json()
         
         inputs = [
-            { "role": "system", "content": f"You are a friendly assistant whose mood is {mood_style} and response you generate will be {language_style} and complete." },
+            { "role": "system", "content": f"Your name is 'Moody' whose mood keep changing. Right now your mood is {mood_style} and response you will generate will be {language_style} and complete." },
             { "role": "user", "content": f"{query}"}
         ]
         output = run("@cf/meta/llama-2-7b-chat-fp16", inputs)
