a43cb3025de283a16c98f3682e1df5c890599512
Aman Rawat <ar.rawataman@gmail.com>
2024-01-19 09:13:52 +0530

    removing build entries for db

--- build_files.sh
+++ build_files.sh
@@ -6,9 +6,9 @@ which npm
 if [ $ENV = "prod" ]; then
 
     echo "Now Running on PRODUCTION environment"
-    python3.9 manage.py migrate
+    # python3.9 manage.py migrate
     python3.9 manage.py collectstatic --noinput
-    python3.9 manage.py createsuperuser --noinput
+    # python3.9 manage.py createsuperuser --noinput
 
 elif [ $ENV = "dev" ]; then
 
