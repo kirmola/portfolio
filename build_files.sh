7202df5d61cac19e05fee50416de90c3332c5f35
Aman Rawat <ar.rawataman@gmail.com>
2024-01-19 07:04:29 +0530

    Changed: build_files.sh for development and production environment

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,18 @@ pip install -r requirements.txt
 which npm
 
 if [ $ENV = "prod" ]; then
+
+    echo "Now Running on PRODUCTION environment"
+
     python3.9 manage.py migrate
     python3.9 manage.py tailwind build
     python3.9 manage.py collectstatic --noinput
     python3.9 manage.py createsuperuser --noinput
+
+elif [ $ENV = "dev" ]; then
+
+    echo "Now Running on DEVELOPMENT environment"
+
+    python3.9 manage.py collectstatic --noinput
 
 fi
