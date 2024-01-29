de13fd154917ab74d7b493e7c9c49f362e0e83b0
Aman Rawat <ar.rawataman@gmail.com>
2024-01-29 17:34:52 +0530

    logon working perfectly

--- logonai/views.py
+++ logonai/views.py
@@ -1,13 +1,16 @@
+from django.utils.text import slugify
+from os import environ
+import aiohttp, asyncio, boto3
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .forms import LogonForm
 import requests
-from django.http import FileResponse
+from django.http import HttpResponse
 from PIL import Image
 from io import BytesIO
 from django.core.files.temp import NamedTemporaryFile
 from portfolio.seoclass import SEOClass
-
+from base64 import b64encode, b64decode
 class LogonIndexView(TemplateView):
     template_name = "logonai/index.html"
 
@@ -28,24 +31,44 @@ class LogonIndexView(TemplateView):
 
 
 class GenerateImageView(TemplateView):
+    content_type = "image/png"
+    template_name = "logonai/index.html"
 
-    def generate_response(self, prompt):
+    async def generate_response(self, prompt):
         API_BASE_URL = "https://logon.amanrawat.workers.dev/"
         inputs = {
             "prompt": prompt,
         }
-        response = requests.post(API_BASE_URL, json=inputs)
-        results = response.content
-        return results
+        async with aiohttp.ClientSession() as session:
+            async with session.post(API_BASE_URL, json=inputs) as response:
+                return await response.read()
+            
+    async def upload_to_s3(self, filename, data):
+            s3 = boto3.resource('s3',
+                endpoint_url=f'https://{environ.get("CF_ACCOUNT_ID")}.r2.cloudflarestorage.com',
+                aws_access_key_id=environ.get("CF_ACCESS_KEY_ID"),
+                aws_secret_access_key=environ.get("CF_SECRET_ACCESS_KEY")
+            )
+            binary_data = b64decode(data)
+            s3.Bucket(environ["BUCKET_NAME"]).put_object(Key=filename, Body=binary_data, Metadata={
+                "Content-Type": "image/png"
+            })
 
-    def post(self, request):
+
+    async def process_image(self, prompt):
+        data = await self.generate_response(prompt)
+        base64_image = b64encode(data).decode("utf-8")
+        filename = f"{slugify(prompt)}.png"
+        try:
+            await self.upload_to_s3(filename, data)
+        except:
+            pass
+        return f'<img src="data:image/png;base64,{base64_image}" alt="Result for {prompt}">'
+
+    async def post(self, request):
         prompt = self.request.POST.get("prompt")
-        response = self.generate_response(prompt)
-        # image = Image.open(BytesIO(response))
-        fname = f"{prompt}.png"
-        with NamedTemporaryFile("wb", prefix=fname) as tempFile:      
-            tempFile.write(response)
-        out = FileResponse(filename=fname, headers={
-            "content-type": "image/png",
-        })
-        return out
+        image_html = await self.process_image(prompt)
+        return HttpResponse(image_html)
+
+    async def get(self, requst):
+        return render(requst, self.template_name)
