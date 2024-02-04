3a664e0ff965812312ab0a71b7a51054767dca24
Aman Rawat <ar.rawataman@gmail.com>
2024-02-04 19:17:26 +0530

    fixed logic here

--- saaya/logics.py
+++ saaya/logics.py
@@ -1,6 +1,6 @@
 import requests, json
 import aiohttp, asyncio
-
+from bs4 import BeautifulSoup
 
 async def getExternalResults(query):
     GOOGLE_API_BASE_URL = f"https://www.google.com/complete/search?q={query}&client=chrome"
@@ -18,12 +18,16 @@ async def getExternalResults(query):
             for each_response in responses:
                 if each_response.status == 200:
                     google_results = await responses[0].text()
-                    bing_results = responses[1]
+                    bing_results = await responses[1].text()
                     ddg_results = await responses[2].text()
+
+                    soup = BeautifulSoup(bing_results, "html.parser")
+                    sa_sg_elements = soup.find_all(class_='sa_sg')
+
                     final_google_res = json.loads(google_results)[1]
-                    # final_bing_res = json.loads()
+                    final_bing_res = [element.get('query') for element in sa_sg_elements]
                     final_ddg_res = json.loads(ddg_results)
-                    final_results = [final_google_res, [i["phrase"] for i in final_ddg_res]]
+                    final_results = [final_google_res, final_bing_res, [i["phrase"] for i in final_ddg_res]]
                 else:
                     final_results.append("Something fishy is going on here, try later!")
         except:
