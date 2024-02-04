817d3d6cac391da046557fc5aea7e007120d39ac
Aman Rawat <ar.rawataman@gmail.com>
2024-02-04 13:32:46 +0530

    update: urls.py and navbar.html. added navbar links

--- saaya/urls.py
+++ saaya/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.saayaIndex, name='index'),
+    path('', views.saayaIndex, name='saaya_home'),
     path('results/', views.getResults, name='getresults')
 ]
