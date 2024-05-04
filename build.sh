8e7085a397749f5ff4da05d08f8cdd29648fb1fe
Aman Rawat <ar.rawataman@gmail.com>
2024-05-04 16:42:24 +0530

    including dev dependencies too

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ curl -sSL https://install.python-poetry.org | python3 -;
 export PATH="/vercel/.local/bin:$PATH";
 poetry install;
 poetry self add poetry-plugin-export
-poetry export --without-hashes > requirements.txt
+poetry export --without-hashes --with dev > requirements.txt
 
 
 # Install from requirements.txt
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Generate CSS
 cd theme/static_src;
