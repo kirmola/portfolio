ecdc10a85cbd4ff0f6c6e7a836f2bc125cf74054
Aman Rawat <ar.rawataman@gmail.com>
2024-05-04 11:29:01 +0530

    testing via req.txt

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 
-# Install Poetry
-curl -sSL https://install.python-poetry.org | python3 -;
-export PATH="/vercel/.local/bin:$PATH";
-poetry install;
-poetry --version
+# # Install Poetry
+# curl -sSL https://install.python-poetry.org | python3 -;
+# export PATH="/vercel/.local/bin:$PATH";
+# poetry install;
+# poetry --version
+
+# Install requirements
+
+pip install -r requirements.txt
 
 # Generate CSS
 cd theme/static_src;
@@ -12,4 +16,4 @@ npm run build;
 cd ../..;
 
 # Generate Static files
-poetry run python3 manage.py collectstatic --noinput;
+python3.12 manage.py collectstatic --noinput;
